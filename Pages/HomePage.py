0d2009aa3404cddfb8c801c192f88d1e3132fefa
simeonzografov <simeon.zografov@hedgehoglab.com>
2024-10-31 14:15:10 +0200

    Update matrix

---
+++ Pages/HomePage.py
@@ -0,0 +1,64 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class HomePage:
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.app_logo = (By.XPATH, "//a[contains(@class,'logo-link')]")
+        self.home_button = (By.XPATH, "//a[.='Home']")
+        self.sites_button = (By.XPATH, "//a[.='Sites']")
+        self.login_button = (By.XPATH, "//a[.='Log in']")
+        self.home_page_title = (By.XPATH, "//h1[.='Bringing the past into the future']")
+        self.sites_page_title = (By.XPATH, "//h1[.='Experience yesterday, today']")
+        self.logout_button = (By.XPATH, "//button[.='Log out']")
+        self.language_switch_button = (By.ID, "lang-switcher")
+        self.language_button = (By.XPATH, "//button[@role='menuitem']")
+
+    def is_app_logo_visible(self):
+        return self.driver.find_element(*self.app_logo).is_displayed()
+
+    def is_home_page_title_visible(self):
+        wait = WebDriverWait(self.driver, 30)
+        page_title = wait.until(EC.visibility_of_element_located(self.home_page_title))
+        return page_title.is_displayed()
+
+    def is_sites_page_title_visible(self):
+        wait = WebDriverWait(self.driver, 30)
+        page_title = wait.until(EC.visibility_of_element_located(self.sites_page_title))
+        return page_title.is_displayed()
+
+    def click_app_logo(self):
+        self.driver.find_element(*self.app_logo).click()
+
+    def click_home_button(self):
+        self.driver.find_element(*self.home_button).click()
+
+    def click_sites_button(self):
+        self.driver.find_element(*self.sites_button).click()
+
+    def click_login_button(self):
+        self.driver.find_element(*self.login_button).click()
+
+    def wait_page_to_load(self):
+        wait = WebDriverWait(self.driver, 30)
+        wait.until(EC.visibility_of_element_located((By.XPATH, "//h1")))
+
+    def is_logout_button_visible(self):
+        wait = WebDriverWait(self.driver, 30)
+        button = wait.until(EC.visibility_of_element_located(self.logout_button))
+        return button.is_displayed()
+
+    def click_logout_button(self):
+        self.driver.find_element(*self.logout_button).click()
+
+    def click_language_switch_button(self):
+        self.driver.find_element(*self.language_switch_button).click()
+
+    def click_language_button(self):
+        self.driver.find_element(*self.language_button).click()
+
+    def get_page_language(self):
+        return self.driver.find_element(By.XPATH, "//html").get_attribute("lang")
---
+++ Pages/LoginPage.py
@@ -0,0 +1,55 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class Login:
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.login_page_title = (By.XPATH, "//h1[.='Log in']")
+        self.email_field = (By.ID, "email")
+        self.password_field = (By.ID, "loginPassword")
+        self.login_button = (By.XPATH, "//form[@class='login-form']//button[.='Log in']")
+        self.field_error_message = (By.XPATH, "//ul[@class='invalid-feedback']/li")
+        self.credentials_error_message = (By.XPATH, "//div[@role='alert']")
+
+    def is_login_page_title_visible(self):
+        wait = WebDriverWait(self.driver, 30)
+        page_title = wait.until(EC.visibility_of_element_located(self.login_page_title))
+        return page_title.is_displayed()
+
+    def click_login_button(self):
+        self.driver.find_element(*self.login_button).click()
+
+    def set_email_field(self, email):
+        self.driver.find_element(*self.email_field).click()
+        self.driver.find_element(*self.email_field).clear()
+        if email != "":
+            self.driver.find_element(*self.email_field).send_keys(email)
+        self.driver.find_element(*self.password_field).click()
+
+    def set_password_field(self, password):
+        self.driver.find_element(*self.password_field).click()
+        self.driver.find_element(*self.password_field).clear()
+        if password != "":
+            self.driver.find_element(*self.password_field).send_keys(password)
+        self.driver.find_element(*self.email_field).click()
+
+    def is_filed_error_message_visible(self):
+        wait = WebDriverWait(self.driver, 30)
+        message = wait.until(EC.visibility_of_element_located(self.field_error_message))
+        return message.is_displayed()
+
+    def get_field_error_message_text(self):
+        return self.driver.find_element(*self.field_error_message).text
+
+    def is_credentials_error_message_visible(self):
+        wait = WebDriverWait(self.driver, 30)
+        message = wait.until(EC.visibility_of_element_located(self.credentials_error_message))
+        return message.is_displayed()
+
+    def get_credentials_error_message_text(self):
+        return self.driver.find_element(*self.credentials_error_message).text
+
+
