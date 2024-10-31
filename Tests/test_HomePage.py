1d0fc7735136c364e55f03920a5f26601225d99e
simeonzografov <simeon.zografov@hedgehoglab.com>
2024-10-31 16:01:52 +0200

    Add browsers

---
+++ Tests/test_HomePage.py
@@ -0,0 +1,47 @@
+import allure
+import pytest
+from pytest_check import check
+from allure import severity, severity_level
+
+from Common import APIRequests
+from Pages.HomePage import HomePage
+from Common.BaseClass import BaseClass
+
+
+@pytest.mark.parametrize("driver", BaseClass.browsers, indirect=True)
+class TestHomePage(BaseClass):
+
+    @severity(severity_level.CRITICAL)
+    @allure.feature('Home page')
+    @allure.title("User is navigated to the Home page")
+    def test_1(self, driver):
+        home_page_obj = HomePage(driver)
+        driver.get(BaseClass.url)
+        with check, allure.step("Check the page title"):
+            assert home_page_obj.is_home_page_title_visible()
+        with check, allure.step("Logo is visible"):
+            assert home_page_obj.is_app_logo_visible()
+
+    @severity(severity_level.CRITICAL)
+    @allure.feature('Home page')
+    @allure.title("Check main navigation")
+    def test_2(self, driver):
+        home_page_obj = HomePage(driver)
+        with check, allure.step("User is navigated to Sites page"):
+            home_page_obj.click_sites_button()
+            assert home_page_obj.is_sites_page_title_visible()
+        with check, allure.step("User is navigated to Home page"):
+            home_page_obj.click_home_button()
+            assert home_page_obj.is_home_page_title_visible()
+
+    @severity(severity_level.NORMAL)
+    @allure.feature('Home page')
+    @allure.title("Check language switch")
+    def test_3(self, driver):
+        home_page_obj = HomePage(driver)
+        with check, allure.step("English is selected by default"):
+            assert home_page_obj.get_page_language() == "en-US"
+        with check, allure.step("Successful language change"):
+            home_page_obj.click_language_switch_button()
+            home_page_obj.click_language_button()
+            assert home_page_obj.get_page_language() == "ar-SA"
