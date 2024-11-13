a7ec20bda90bf51294f54a6503b902f88f5abb0f
simeonzografov <simeon.zografov@hedgehoglab.com>
2024-11-13 10:01:09 +0200

    Proxy fix

---
+++ Common/BaseClass.py
@@ -0,0 +1,208 @@
+import os
+import shutil
+import subprocess
+import time
+
+import pytest
+from dotenv import load_dotenv
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium import webdriver
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.safari.options import Options as SafariOptions
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+
+class BaseClass:
+    load_dotenv()
+    url = os.getenv("URL")
+    password = os.getenv("PASSWORD")
+    browsers = os.getenv("BROWSERS")
+    email = os.getenv("EMAIL")
+    email_password = os.getenv("EMAIL_PASSWORD")
+    browsers = browsers.split(", ")
+    api_url = os.getenv("APIURL")
+    kcurl = os.getenv("KCURL")
+    client_secret = os.getenv("CLIENT_SECRET")
+    cms_url = os.getenv("CMS_URL")
+    cms_email = os.getenv("CMS_EMAIL")
+    cms_password = os.getenv("CMS_PASSWORD")
+
+    @pytest.fixture(scope="class", autouse=True)
+    def driver(self, request):
+        browser = request.param
+
+        project_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+        is_ci = os.getenv('CI') == 'true'
+        if is_ci:
+            if browser == "edge":
+                # Set up Edge options
+                options = EdgeOptions()
+                options.add_argument("--headless")
+                options.add_argument("--disable-gpu")
+                options.add_argument("--no-sandbox")
+                options.add_argument("--disable-dev-shm-usage")
+                options.add_argument("--disable-extensions")
+                options.add_argument("--disable-infobars")
+                serv = EdgeService(EdgeChromiumDriverManager().install())
+                driver = webdriver.Edge(service=serv, options=options)
+            elif browser == "chrome":
+                options = ChromeOptions()
+                options.add_argument("--headless")
+                options.add_argument("--disable-gpu")
+                options.add_argument("--no-sandbox")
+                options.add_argument("--disable-dev-shm-usage")
+                options.add_argument("--disable-extensions")
+                options.add_argument("--disable-infobars")
+                '''chrome_driver_path = "/usr/local/share/chrome_driver/chromedriver"
+                serv = ChromeService(executable_path=chrome_driver_path)
+                driver = webdriver.Chrome(service=serv, options=options)'''
+                chrome_driver_path = "/usr/bin/chromedriver"
+                serv = ChromeService(chrome_driver_path)
+                driver = webdriver.Chrome(service=serv, options=options)
+            elif browser == "firefox":
+                options = FirefoxOptions()
+                options.add_argument("--headless")
+                geckodriver_driver_path = "/usr/bin/geckodriver"
+                serv = FirefoxService(geckodriver_driver_path)
+                driver = webdriver.Firefox(service=serv, options=options)
+                # options.headless = True
+                # driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
+            else:
+                options = SafariOptions()
+                options.page_load_strategy = 'eager'
+                driver = webdriver.Safari(options=options)
+        else:
+            if browser == "edge":
+                # edge_driver_path = os.path.join(project_folder, 'Resources', 'msedgedriver')
+                # serv = EdgeService(edge_driver_path)
+                # driver = webdriver.Edge(service=serv)
+                driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+            elif browser == "chrome":
+                chrome_driver_path = os.path.join(project_folder, 'Resources', 'chromedriver')
+                serv = ChromeService(chrome_driver_path)
+                driver = webdriver.Chrome(service=serv)
+                # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+            elif browser == "firefox":
+                # firefox_driver_path = os.path.join(project_folder, 'Resources', 'geckodriver')
+                # serv = FirefoxService(firefox_driver_path)
+                # driver = webdriver.Firefox(service=serv)
+                driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+            else:
+                options = SafariOptions()
+                options.page_load_strategy = 'eager'
+                driver = webdriver.Safari(options=options)
+
+        driver.implicitly_wait(10)
+        driver.maximize_window()
+        yield driver
+        driver.quit()
+
+    @pytest.fixture(scope="function", autouse=False)
+    def proxy_driver(self, request):
+        browser = request.node.callspec.params["driver"]
+        test_name = request.param
+        print(browser)
+        print(test_name)
+        # browser = getattr(request, "param", None)
+        project_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        mitmdump_path = shutil.which("mitmdump")
+        script_path = os.path.join(project_folder, "Common", "ResponseInterception.py")
+        print(script_path)
+
+        if mitmdump_path is None:
+            raise FileNotFoundError("mitmdump executable not found in PATH. Please ensure mitmproxy is installed.")
+        if browser == "chrome":
+            port = "8082"
+        elif browser == "edge":
+            port = "8084"
+        else:
+            port = "8081"
+        # port = "8082"
+        mitmdump_process = subprocess.Popen([mitmdump_path, "-s", script_path, "--listen-port", port,
+                                             "--set", f"test_name={test_name}"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        time.sleep(5)
+        if mitmdump_process:
+            print("Proxy subprocess started")
+        if os.getenv('CI') == 'true':
+            if browser == "chrome":
+                options = ChromeOptions()
+                options.add_argument("--headless")
+                options.add_argument("--disable-gpu")
+                options.add_argument("--no-sandbox")
+                options.add_argument("--disable-dev-shm-usage")
+                options.add_argument("--disable-extensions")
+                options.add_argument("--disable-infobars")
+                options.add_argument(f'--proxy-server=http://127.0.0.1:{port}')
+                options.add_argument('--ignore-certificate-errors')
+                chrome_driver_path = "/usr/bin/chromedriver"
+                serv = ChromeService(chrome_driver_path)
+                proxy_driver = webdriver.Chrome(service=serv, options=options)
+            elif browser == "edge":
+                options = EdgeOptions()
+                options.add_argument("--headless")
+                options.add_argument("--disable-gpu")
+                options.add_argument("--no-sandbox")
+                options.add_argument("--disable-dev-shm-usage")
+                options.add_argument("--disable-extensions")
+                options.add_argument("--disable-infobars")
+                options.add_argument(f'--proxy-server=http://127.0.0.1:{port}')
+                options.add_argument('--ignore-certificate-errors')
+                serv = EdgeService(EdgeChromiumDriverManager().install())
+                proxy_driver = webdriver.Edge(service=serv, options=options)
+            else:
+                pytest.skip("Unsupported on the browser")
+        else:
+            if browser == "chrome":
+                chrome_driver_path = os.path.join(project_folder, 'Resources', 'chromedriver')
+                options = webdriver.ChromeOptions()
+                options.add_argument(f'--proxy-server=http://127.0.0.1:{port}')  # mitmproxy default proxy
+                options.add_argument('--ignore-certificate-errors')  # Bypass cert errors if needed for testing
+
+                serv = ChromeService(chrome_driver_path)
+                proxy_driver = webdriver.Chrome(service=serv, options=options)
+            elif browser == "edge":
+                options = webdriver.EdgeOptions()
+                options.add_argument(f'--proxy-server=http://127.0.0.1:{port}')  # mitmproxy default proxy
+                options.add_argument('--ignore-certificate-errors')  # Bypass cert errors if needed for testing
+
+                proxy_driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()), options=options)
+            else:
+                pytest.skip("Unsupported on the browser")
+            # elif browser == "firefox":
+            #     options = FirefoxOptions()
+            #     # firefox_profile = webdriver.FirefoxProfile()
+            #     # Specify to use manual proxy configuration.
+            #     options.set_preference('network.proxy.type', 1)
+            #     # Set the host/port.
+            #     options.set_preference('network.proxy.http', 'http://127.0.0.1')
+            #     options.set_preference('network.proxy.https_port', port)
+            #     options.set_preference('network.proxy.ssl', 'http://127.0.0.1')
+            #     options.set_preference('network.proxy.ssl_port', port)
+            #     # options.add_argument(f'--proxy-server=http://127.0.0.1:{port}')
+            #     # options.set_preference("security.enterprise_roots.enabled", True)
+            #     # options.set_preference("network.proxy.allow_hijacking_localhost", True)
+            #     # options.set_preference("devtools.console.stdout.content", True)
+            #
+            #     proxy_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
+            # else:
+            #     options = SafariOptions()
+            #     options.page_load_strategy = 'eager'
+            #     proxy_driver = webdriver.Safari(options=options)
+        print("Proxy driver created")
+        proxy_driver.implicitly_wait(10)
+        proxy_driver.maximize_window()
+        yield proxy_driver
+        proxy_driver.quit()
+        mitmdump_process.terminate()
+        mitmdump_process.wait()
---
+++ Pages/HomePage.py
@@ -0,0 +1,330 @@
+import os
+import re
+import time
+import pytz
+from datetime import datetime
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class HomePage:
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.hero_banner = (By.XPATH, "//section[@class='app-hero']")
+        self.app_download_section = (By.XPATH, "//section[@class='app-download']")
+        self.site_carousel_section = (By.XPATH, "(//section[@class='container'])[1]")
+        self.event_carousel_section = (By.XPATH, "(//section[@class='container'])[2]")
+        self.app_footer = (By.XPATH, "//div[@class='app-footer']")
+        self.hero_banner_title = (By.XPATH, "//h1")
+        self.hero_banner_subtitle = (By.XPATH, "//h1/..//p")
+        self.hero_banner_button = (By.XPATH, "//h1/..//a")
+        self.download_title = (By.XPATH, "//section[@class='app-download']//h2")
+        self.download_description = (By.XPATH, "//section[@class='app-download']//p")
+        self.download_image = (By.XPATH, "//section[@class='app-download']//div/img")
+        self.download_stats_section = (By.XPATH, "//section[@class='app-download']//ul")
+        self.download_stat_value = (By.XPATH, "//section[@class='app-download']//li/strong")
+        self.download_stat_description = (By.XPATH, "//section[@class='app-download']//li/span")
+        self.download_google_button = (By.XPATH, "//section[@class='app-download']//a[contains(@href, 'google')]")
+        self.download_apple_button = (By.XPATH, "//section[@class='app-download']//a[contains(@href, 'apple')]")
+        self.site_carousel_title = (By.XPATH, "(//section[@class='container'])[1]//h2")
+        self.site_carousel_description = (By.XPATH, "(//section[@class='container'])[1]//div[@class='description']")
+        self.site_carousel_link = (By.XPATH, "(//section[@class='container'])[1]//a[@class='icon-text']")
+        self.site_carousel_card = (By.XPATH, "(//section[@class='container'])[1]//div[@class='card app-carousel-card']")
+        self.site_carousel_card_titles = (By.XPATH, "(//section[@class='container'])[1]//div[@id='carousel']//h4")
+        self.site_carousel_card_descriptions = (By.XPATH, "(//section[@class='container'])[1]//div[@id='carousel']//p")
+        self.site_carousel_card_images = (By.XPATH, "(//section[@class='container'])[1]//div[@id='carousel']//img")
+        self.event_carousel_title = (By.XPATH, "(//section[@class='container'])[2]//h2")
+        self.event_carousel_description = (By.XPATH, "(//section[@class='container'])[2]//div[@class='description']")
+        self.event_carousel_link = (By.XPATH, "(//section[@class='container'])[2]//a[@class='icon-text']")
+        self.event_carousel_card = (By.XPATH, "(//section[@class='container'])[2]//div[@class='card app-carousel-card']")
+        self.event_carousel_card_titles = (By.XPATH, "(//section[@class='container'])[2]//div[@id='carousel']//h4")
+        self.event_carousel_card_descriptions = (By.XPATH, "(//section[@class='container'])[2]//div[@id='carousel']//p")
+        self.event_carousel_card_images = (By.XPATH, "(//section[@class='container'])[2]//div[@id='carousel']//img")
+        self.event_carousel_card_time = (By.XPATH, "(//section[@class='container'])[2]//div[@class='card-footer']")
+
+    def is_home_page_title_visible(self, title):
+        wait = WebDriverWait(self.driver, 30)
+        page_title = wait.until(EC.visibility_of_element_located((By.XPATH, f"//h1[.='{title}']")))
+        return page_title.is_displayed()
+
+    def is_hero_banner_visible(self):
+        return self.driver.find_element(*self.hero_banner).is_displayed()
+
+    def is_app_download_section_visible(self):
+        return self.driver.find_element(*self.app_download_section).is_displayed()
+
+    def is_site_carousel_section_visible(self):
+        return self.driver.find_element(*self.site_carousel_section).is_displayed()
+
+    def is_event_carousel_section_visible(self):
+        return self.driver.find_element(*self.event_carousel_section).is_displayed()
+
+    def is_app_footer_visible(self):
+        return self.driver.find_element(*self.app_footer).is_displayed()
+
+    def is_hero_banner_title_visible(self):
+        return self.driver.find_element(*self.hero_banner_title).is_displayed()
+
+    def get_hero_banner_title_text(self):
+        return self.driver.find_element(*self.hero_banner_title).text
+
+    def is_hero_banner_subtitle_visible(self):
+        return self.driver.find_element(*self.hero_banner_subtitle).is_displayed()
+
+    def get_hero_banner_subtitle_text(self):
+        return self.driver.find_element(*self.hero_banner_subtitle).text
+
+    def is_hero_banner_button_visible(self):
+        return self.driver.find_element(*self.hero_banner_button).is_displayed()
+
+    def get_hero_banner_button_text(self):
+        return self.driver.find_element(*self.hero_banner_button).text
+
+    def click_hero_banner_button(self):
+        self.driver.find_element(*self.hero_banner_button).click()
+
+    def get_hero_banner_image(self):
+        image = self.driver.find_element(*self.hero_banner).get_attribute("style")
+        filename = re.search(r'([^/]+?\.[a-zA-Z0-9]+)(?=\);|$)', image)
+        if filename:
+            filename = filename.group(0)
+        return filename.replace("\\", "")
+
+    def is_download_title_visible(self):
+        return self.driver.find_element(*self.download_title).is_displayed()
+
+    def get_download_title_text(self):
+        return self.driver.find_element(*self.download_title).text
+
+    def is_download_description_visible(self):
+        elements = self.driver.find_elements(*self.download_description)
+        if len(elements) > 0:
+            return True
+        else:
+            return False
+
+    def get_download_description_text(self):
+        description_text = ""
+        elements = self.driver.find_elements(*self.download_description)
+        for element in elements:
+            description_text = description_text + element.text.strip()
+        return description_text
+
+    def is_download_image_visible(self):
+        return self.driver.find_element(*self.download_image).is_displayed()
+
+    def get_download_image(self):
+        image = self.driver.find_element(*self.download_image).get_attribute("src")
+        filename = re.search(r'([^/]+?\.[a-zA-Z0-9]+)(?=\);|$)', image)
+        if filename:
+            filename = filename.group(0)
+        return filename
+
+    def is_download_stats_section_visible(self):
+        return self.driver.find_element(*self.download_stats_section).is_displayed()
+
+    def get_download_stats(self, num):
+        values = self.driver.find_elements(*self.download_stat_value)
+        descriptions = self.driver.find_elements(*self.download_stat_description)
+        value = values[num].text
+        description = descriptions[num].text
+        return value, description
+
+    def get_stats_pairs_number(self):
+        values = self.driver.find_elements(*self.download_stat_value)
+        return len(values)
+
+    def is_download_google_play_button_visible(self):
+        return self.driver.find_element(*self.download_google_button).is_displayed()
+
+    def get_download_google_play_button_url(self):
+        return self.driver.find_element(*self.download_google_button).get_attribute('href')
+
+    def is_download_app_store_button_visible(self):
+        return self.driver.find_element(*self.download_apple_button).is_displayed()
+
+    def get_download_app_store_button_url(self):
+        return self.driver.find_element(*self.download_apple_button).get_attribute('href')
+
+    def is_site_carousel_title_visible(self):
+        return self.driver.find_element(*self.site_carousel_title).is_displayed()
+
+    def get_site_carousel_title_text(self):
+        return self.driver.find_element(*self.site_carousel_title).text
+
+    def is_site_carousel_description_visible(self):
+        return self.driver.find_element(*self.site_carousel_description).is_displayed()
+
+    def get_site_carousel_description_text(self):
+        return self.driver.find_element(*self.site_carousel_description).text
+
+    def is_site_carousel_button_visible(self):
+        return self.driver.find_element(*self.site_carousel_link).is_displayed()
+
+    def get_site_carousel_button_text(self):
+        return self.driver.find_element(*self.site_carousel_link).text
+
+    def click_site_carousel_button(self):
+        element = self.driver.find_element(*self.site_carousel_link)
+        location = element.location
+        x = location['x']
+        y = location['y']
+        self.driver.execute_script(f"window.scrollTo({x}, {y - 150});")
+        time.sleep(1)
+        element.click()
+
+    def is_site_carousel_cards_visible(self):
+        elements = self.driver.find_elements(*self.site_carousel_card_titles)
+        if len(elements) > 0:
+            return True
+        else:
+            return False
+
+    def get_site_carousel_cards_number(self):
+        elements = self.driver.find_elements(*self.site_carousel_card_titles)
+        return len(elements)
+
+    def get_site_carousel_card_title(self, num):
+        card_titles = self.driver.find_elements(*self.site_carousel_card_titles)
+        card_title = card_titles[num].text
+        return card_title
+
+    def get_site_carousel_card_description(self, num):
+        card_descriptions = self.driver.find_elements(*self.site_carousel_card_descriptions)
+        card_description = card_descriptions[num].text
+        return card_description
+
+    def get_site_carousel_card_image(self, num):
+        card_images = self.driver.find_elements(*self.site_carousel_card_images)
+        card_image = card_images[num].get_attribute('src')
+        filename = re.search(r'([^/]+?\.[a-zA-Z0-9]+)(?=\);|$)', card_image)
+        if filename:
+            filename = filename.group(0)
+        return filename.replace("%20", " ")
+
+    def click_site_carousel_card(self, num):
+        cards = self.driver.find_elements(*self.site_carousel_card)
+        element = cards[num]
+        location = element.location
+        x = location['x']
+        y = location['y']
+        self.driver.execute_script(f"window.scrollTo({x}, {y - 150});")
+        time.sleep(1)
+        element.click()
+
+    def is_event_carousel_title_visible(self):
+        return self.driver.find_element(*self.event_carousel_title).is_displayed()
+
+    def get_event_carousel_title_text(self):
+        return self.driver.find_element(*self.event_carousel_title).text
+
+    def is_event_carousel_description_visible(self):
+        return self.driver.find_element(*self.event_carousel_description).is_displayed()
+
+    def get_event_carousel_description_text(self):
+        return self.driver.find_element(*self.event_carousel_description).text
+
+    def is_event_carousel_button_visible(self):
+        return self.driver.find_element(*self.event_carousel_link).is_displayed()
+
+    def get_event_carousel_button_text(self):
+        return self.driver.find_element(*self.event_carousel_link).text
+
+    def click_event_carousel_button(self):
+        element = self.driver.find_element(*self.event_carousel_link)
+        location = element.location
+        x = location['x']
+        y = location['y']
+        self.driver.execute_script(f"window.scrollTo({x}, {y - 150});")
+        time.sleep(1)
+        element.click()
+
+    def is_event_carousel_cards_visible(self):
+        elements = self.driver.find_elements(*self.event_carousel_card_titles)
+        if len(elements) > 0:
+            return True
+        else:
+            return False
+
+    def get_event_carousel_cards_number(self):
+        elements = self.driver.find_elements(*self.event_carousel_card_titles)
+        return len(elements)
+
+    def get_event_carousel_card_title(self, num):
+        card_titles = self.driver.find_elements(*self.event_carousel_card_titles)
+        card_title = card_titles[num].text
+        return card_title
+
+    def get_event_carousel_card_description(self, num):
+        card_descriptions = self.driver.find_elements(*self.event_carousel_card_descriptions)
+        card_description = card_descriptions[num].text
+        return card_description
+
+    def get_event_carousel_card_image(self, num):
+        card_images = self.driver.find_elements(*self.event_carousel_card_images)
+        card_image = card_images[num].get_attribute('src')
+        filename = re.search(r'([^/]+?\.[a-zA-Z0-9]+)(?=\);|$)', card_image)
+        if filename:
+            filename = filename.group(0)
+        return filename.replace("%20", " ")
+
+    def click_event_carousel_card(self, num):
+        cards = self.driver.find_elements(*self.event_carousel_card)
+        element = cards[num]
+        location = element.location
+        x = location['x']
+        y = location['y']
+        self.driver.execute_script(f"window.scrollTo({x}, {y - 150});")
+        time.sleep(1)
+        element.click()
+
+    def get_event_carousel_card_time(self, num):
+        card_times = self.driver.find_elements(*self.event_carousel_card_time)
+        card_time = card_times[num].text
+        return card_time
+
+    @staticmethod
+    def get_formatted_time(start_iso, end_iso):
+        start_dt = datetime.fromisoformat(start_iso.replace("Z", "+00:00"))
+        end_dt = datetime.fromisoformat(end_iso.replace("Z", "+00:00"))
+        if os.getenv('CI') == 'true':
+            print("CI execution")
+            hosted_tz = pytz.timezone("Europe/London")
+        else:
+            hosted_tz = pytz.timezone("Europe/Sofia")
+        ast_tz = pytz.timezone("Asia/Riyadh")
+        start_local = start_dt.astimezone(hosted_tz)
+        end_local = end_dt.astimezone(hosted_tz)
+        start_ast = start_dt.astimezone(ast_tz)
+        end_ast = end_dt.astimezone(ast_tz)
+        local_format = "{:%-d %B, %-I:%M%p}".format
+        ast_format = "{:%-d %B, %-I:%M%p}".format
+        start_local_str = local_format(start_local)
+        end_local_str = local_format(end_local)
+        start_ast_str = ast_format(start_ast)
+        end_ast_str = ast_format(end_ast)
+        result = f"{start_local_str} - {end_local_str} ({start_ast_str} - {end_ast_str} AST)"
+        return result.replace("AM", "am").replace("PM", "pm")
+
+    @staticmethod
+    def get_last_three_chronological_ordered_events(event_list):
+        def parse_date(date_str):
+            return datetime.fromisoformat(date_str.replace("Z", "+00:00"))
+
+        sorted_events = sorted(event_list.items(), key=lambda x: parse_date(x[1]['start']))
+        valid_titles = []
+        now = datetime.now(pytz.UTC)
+        for event_id, event in sorted_events:
+            start_dt = parse_date(event['start'])
+            end_dt = parse_date(event['end'])
+            if start_dt < now and end_dt < now:
+                continue
+            if start_dt >= end_dt:
+                continue
+            valid_titles.append(event['title'])
+            if len(valid_titles) == 3:
+                break
+        return valid_titles
+
