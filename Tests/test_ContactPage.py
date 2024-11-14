c02e6d148a24f469b5aefb1490fafa8ae0cf200f
simeonzografov <simeon.zografov@hedgehoglab.com>
2024-11-14 09:51:45 +0200

    Firefox proxy fix

---
+++ Tests/test_ContactPage.py
@@ -0,0 +1,53 @@
+import time
+import allure
+import pytest
+from pytest_check import check
+from allure import severity, severity_level
+from Common import Email
+from Pages.ContactPage import ContactPage
+from Pages.MainNavigation import MainNavigation
+from Common.BaseClass import BaseClass
+
+
+@pytest.mark.parametrize("driver", BaseClass.browsers, indirect=True)
+class TestContactPage(BaseClass):
+    current_browser = None
+
+    @pytest.fixture(autouse=True)
+    def setup(self, request):
+        TestContactPage.current_browser = request.node.callspec.params["driver"]
+
+    @severity(severity_level.CRITICAL)
+    @allure.feature('Contact page')
+    @allure.title("User is navigated to the Contact page")
+    @allure.issue("QP-271", "QP-271")
+    @allure.issue("QP-356", "QP-356")
+    @allure.testcase("58594", "C58594")
+    def test_1(self, driver):
+        contact_obj = ContactPage(driver)
+        main_nav_obj = MainNavigation(driver)
+        driver.get(BaseClass.url)
+        main_nav_obj.wait_page_to_load(TestContactPage.current_browser)
+        contact_obj.click_contact_button()
+        with check, allure.step("Check the page title"):
+            assert contact_obj.is_contact_page_title_visible()
+
+    @severity(severity_level.CRITICAL)
+    @allure.feature('Contact page')
+    @allure.title("Successful message send")
+    def test_2(self, driver):
+        contact_obj = ContactPage(driver)
+        random_name = contact_obj.generate_random_name()
+        random_email = TestContactPage.current_browser + "_" + contact_obj.generate_random_email()
+        random_message = contact_obj.generate_random_message()
+        contact_obj.set_name_field(random_name)
+        contact_obj.set_email_field(random_email)
+        contact_obj.set_message_field(random_message)
+        contact_obj.click_send_message_button()
+        time.sleep(15)
+        with check, allure.step("Success message is visible"):
+            assert contact_obj.is_success_message_visible()
+        expected_email = f"Full name: {random_name}\r\nEmail address: {random_email}\r\nYour message: {random_message}\r\n"
+        actual_email = Email.get_latest_email_content(TestContactPage.current_browser)
+        with check, allure.step("Email is correct"):
+            assert actual_email == expected_email
